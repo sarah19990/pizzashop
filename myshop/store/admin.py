725acec359519a045972a9a4b7b43cebeb7e443d
sarah19990 <sara1999200407@gmail.com>
2023-05-05 09:53:45 +0100

    creating models for the application

--- myshop/store/admin.py
+++ myshop/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category,Customer,Products,Order
 
-# Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Products,AdminProduct)
+admin.site.register(Order)
