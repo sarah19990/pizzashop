7286e27283b69c0fd25241650e4fbacbd152e3df
sarah19990 <sara1999200407@gmail.com>
2023-05-05 10:20:57 +0100

    added urls to the pages and created the views

---
+++ myshop/store/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Index , store
+from .views import Signup
+from .views import Login , logout
+from .views import Cart
+from .views import CheckOut
+from .views import OrderView
+from .views import search
+from .middlewares.auth import  auth_middleware
+
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('store', store , name='store'),
+
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout , name='logout'),
+    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
+    path('check-out', CheckOut.as_view() , name='checkout'),
+    path('search', search ,name='search'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+
+]
